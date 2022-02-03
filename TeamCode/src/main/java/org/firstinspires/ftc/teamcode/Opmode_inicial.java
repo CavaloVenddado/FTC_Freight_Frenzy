f44599b6a75d1340d39da6f04b2753361b47f791
Cavalo vendado <cavalo.vendado@gmail.com>
2022-02-03 15:34:10 -0300

    add opmode e também cinemática do braço

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode_inicial.java
@@ -0,0 +1,141 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+public class Opmode_inicial extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotorEx Carrossel = null;
+    private DcMotor motorFrontLeft = null;
+    private DcMotor motorBackLeft = null;
+    private DcMotor motorFrontRight = null;
+    private DcMotor motorBackRight = null;
+    private DcMotor motorOmbro = null;
+    private DcMotor motorCotovelo = null;
+    private Servo   servoPulso = null;
+    private Servo   servoGarra = null;
+    private double PosY;
+    private double PosX;
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        Carrossel = hardwareMap.get(DcMotorEx.class, "Carrossel");
+        motorFrontLeft = hardwareMap.get(DcMotorEx.class,"motorFrontLeft");
+        motorBackLeft = hardwareMap.get(DcMotorEx.class,"motorBackLeft");
+        motorFrontRight = hardwareMap.get(DcMotorEx.class,"motorFrontRight");
+        motorBackRight = hardwareMap.get(DcMotorEx.class,"motorBackRight");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        Carrossel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackRight.setDirection(DcMotorEx.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotorEx.Direction.REVERSE);
+        // Wait for the game to start (driver presses PLAY)
+        Robotic_Arm braco = new Robotic_Arm();
+        waitForStart();
+        runtime.reset();
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            if (gamepad1.b==true) {
+                Carrossel.setVelocity(-2500);
+            }else{
+                Carrossel.setVelocity(0);
+            }
+            double velocity = gamepad1.right_trigger;
+            double y = gamepad1.left_stick_y*velocity;
+            double x = gamepad1.left_stick_x*-1.1*velocity;
+            double rx = -gamepad1.right_stick_x*velocity;
+
+            double y2 = gamepad2.right_stick_y;
+            double x2 = gamepad2.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y+x+rx) / denominator;
+            double backLeftPower = (y-x+rx) / denominator;
+            double frontRightPower = (y-x-rx) / denominator;
+            double backRightPower = (y+x-rx) / denominator;
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+
+            PosY = PosY + gamepad2.right_stick_y;
+            PosX = PosX + gamepad2.right_stick_x;
+
+            if (PosY > 0.1){
+                PosY = 0.1;
+            }
+            if (PosY < -0.1){
+                PosY = -0.1;
+            }
+
+            braco.setPos(PosX, PosY);
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+            telemetry.update();
+
+        }
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotic_Arm.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Robotic_Arm {
+    /**
+     * codigo escrito pela equipe cavalo vendado 16786 temporada freght frenzy dia 2/2/2022
+     * este codigo realiza a cinemática de um  braço mecanico de 2 eixos utilizando trigonometria
+     *
+     */
+
+    double a = 0.3; //distancia entre o cotovelo e o pulso
+    double b = 0.304; //distancia entre o ombro e o cotovelo
+    private double Ma;
+    private double angC;
+    public void setPos(double x2, double y2) { //inicio da funcao cinematica
+        double c = Math.sqrt((x2 * x2) + (y2 * y2)); //calculo da hipotenusa entre o ombro e pulso
+        double angA = Math.acos((b * b) + (c * c) - (a * a) / (2 * b * c)); //calculo angulo ombro com hipotenusa
+        angC = Math.acos((a * a) + (b * b) - (c * c) / (2 * a * b)); //calculo angulo cotovelo
+        double alfa = Math.atan(y2 / x2); //calculo do angulo hipotenusa com o chao
+        Ma = 180 - (angA + alfa); //angulo do ombro com o chao
+    }
+    public double getA(){
+        return Ma;
+    }
+    public double getC(){
+        return angC;
+    }
+}
