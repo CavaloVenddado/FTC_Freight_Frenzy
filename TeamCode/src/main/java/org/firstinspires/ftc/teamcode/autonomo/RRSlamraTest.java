4dd6708bf3f64455ce6e9b110414e7744513d938
felipetrentin <felipetrentintecnico@hotmail.com>
2022-04-01 16:08:28 -0300

    Add t265localizer and calib opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomo/RRSlamraTest.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.autonomo;
+
+public class RRSlamraTest {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -143,6 +143,7 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         // TODO: if desired, use setLocalizer() to change the localization method
         // for instance, setLocalizer(new ThreeTrackingWheelLocalizer(...));
+        setLocalizer(new T265Localizer(hardwareMap));
 
         trajectorySequenceRunner = new TrajectorySequenceRunner(follower, HEADING_PID);
     }
